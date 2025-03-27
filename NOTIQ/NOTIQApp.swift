e142485e75864c2ffd53fa7b72481802c5c7056f
mailapham <embe01@icloud.com>
2025-03-27 15:49:23 -0700

    added default files

---
+++ NOTIQ/NOTIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  NOTIQApp.swift
+//  NOTIQ
+//
+//  Created by Maila Pham on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct NOTIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
